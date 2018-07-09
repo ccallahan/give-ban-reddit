9ac202d3d25df3c9e3ffffd83833710e443efdc0
Chance Callahan <chance1callahan@gmail.com>
2018-07-09 09:49:38 -0400

    MAGIC

---
+++ give-ban-reddit/magic.js
@@ -0,0 +1,5 @@
+﻿var comment = document.getElementsByClassName("md");
+
+for (var i = 0, l = comment.length; i < l; i++) {
+    comment[i].innerHTML = comment[i].innerHTML.replace(/<p>(.|\n)*?<\/p>/g, '<p>༼ つ ◕_ ◕ ༽つ GIVE BAN ༼ つ ◕_ ◕ ༽つ</p>');
+}
